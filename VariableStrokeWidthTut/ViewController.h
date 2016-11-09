34208daba6df7383194e4cc8029b23b1ed0dc962
Le Tan Thang <thanglt@administrators-macbook-pro.local>
2016-11-09 11:42:12 +0700

    Initial Commit

---
+++ VariableStrokeWidthTut/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VariableStrokeWidthTut
+//
+//  Created by Le Tan Thang on 11/9/16.
+//  Copyright © 2016 Le Tan Thang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ VariableStrokeWidthTut/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  VariableStrokeWidthTut
+//
+//  Created by Le Tan Thang on 11/9/16.
+//  Copyright © 2016 Le Tan Thang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
